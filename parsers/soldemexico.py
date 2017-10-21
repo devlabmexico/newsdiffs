19eecfd9711727562d5df735970800bc11e97afd
Hugo Aguirre <bul.ikana@gmail.com>
2017-10-20 17:23:16 -0700

    Add sol de mexico parser

--- parsers/soldemexico.py
+++ parsers/soldemexico.py
@@ -53,3 +53,43 @@ class ElSolDeMexicoParser(BaseParser):
                     'https://www.elsoldemexico.com.mx/deportes/futbol',
                     'https://www.elsoldemexico.com.mx/deportes/tenis',
                    ]
+
+    def _parse(self, html):
+        soup = bs4.BeautifulSoup(html)
+
+        elt_title = soup.find('h1', class_="title")
+        if elt_title is None:
+            self.real_article = False
+            return
+        else:
+            self.title = elt_title.getText()
+
+        elt_body = soup.find('section', class_='col-sm-8')
+        if elt_body is None:
+            self.real_article = False
+            return
+        else: 
+            print elt_body.getText()
+            elt_body_content = ''
+            elt_body_ps = elt_body.findAll('p', class_=None)
+            for p in elt_body_ps:
+                elt_body_content = elt_body_content + "\n" + p.getText()
+
+            elt_summary = soup.find('h3', class_='subtitle')
+            if elt_summary is None:
+                self.body = elt_body_content
+            else:
+                self.body = elt_summary.getText() + "\n" + elt_body_content
+
+        
+        elt_byline = soup.find('p', class_='byline')
+        if elt_byline is None:
+            self.byline = ''
+        else:
+            self.byline = elt_byline.getText()
+
+        elt_date = soup.find('p', class_='published-date')
+        if elt_date is None:
+            self.date = ''
+        else:
+            self.date = elt_date.getText()
