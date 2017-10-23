46253d9bcd2306d830e3a4887fe2e65d13c00240
Hugo Aguirre <bul.ikana@gmail.com>
2017-10-23 13:17:40 -0700

    Add test parser to frontend

--- website/frontend/models.py
+++ website/frontend/models.py
@@ -17,12 +17,7 @@ def strip_prefix(string, prefix):
         string = string[len(prefix):]
     return string
 
-PublicationDict = {'www.nytimes.com': 'NYT',
-                   'edition.cnn.com': 'CNN',
-                   'www.bbc.co.uk': 'BBC',
-                   'www.politico.com': 'Politico',
-                   'www.washingtonpost.com': 'Washington Post',
-                   }
+PublicationDict = {'hugo.mecabotware.com': 'TEST'}
 
 ancient = datetime(1901, 1, 1)
 
