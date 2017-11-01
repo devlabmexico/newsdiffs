f739254725dce32ca95d45a24c9e92bee094106c
Hugo Aguirre <bul-ikana@users.noreply.github.com>
2017-11-01 00:08:58 -0600

    Missing comma
    IKR

--- website/frontend/models.py
+++ website/frontend/models.py
@@ -18,7 +18,7 @@ def strip_prefix(string, prefix):
     return string
 
 PublicationDict = {
-                   'www.eluniversal.com.mx': 'El Universal'
+                   'www.eluniversal.com.mx': 'El Universal',
                    'www.excelsior.com.mx': 'Excelsior',
                    'hugo.mecabotware.com': 'TEST',
                    'www.jornada.unam.mx': 'La Jornada en Linea'
