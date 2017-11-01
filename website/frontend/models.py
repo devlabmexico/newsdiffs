be68c9d16f8829ae954f92c9a38e17aeca1bf4ed
Hugo Aguirre <bul.ikana@gmail.com>
2017-11-01 12:55:42 -0600

    Add el sol de mexico to parsers

--- website/frontend/models.py
+++ website/frontend/models.py
@@ -17,11 +17,8 @@ def strip_prefix(string, prefix):
         string = string[len(prefix):]
     return string
 
-PublicationDict = {'www.nytimes.com': 'NYT',
-                   'edition.cnn.com': 'CNN',
-                   'www.bbc.co.uk': 'BBC',
-                   'www.politico.com': 'Politico',
-                   'www.washingtonpost.com': 'Washington Post',
+PublicationDict = {
+                   'www.elsoldemexico.com.mx': 'El Sol de México',
                    }
 
 ancient = datetime(1901, 1, 1)
